df9c7110c36247125f5d2d559dd4b75c3ff16016
hardik rathod <hardikrathod8153@gmail.com>
2025-04-24 18:28:28 +0530

    add netlify.toaml

--- frontend/netlify/functions/myFunction.js
+++
@@ -1,9 +0,0 @@
-// frontend/netlify/functions/myFunction.js
-
-exports.handler = async (event, context) => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Hello from serverless function!" }),
-    };
-  };
-  
