f3b62364754de88d6d54879a1f89be71c27fa26c
hardik rathod <hardikrathod8153@gmail.com>
2025-04-24 17:26:05 +0530

    dist file add to configure

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Make sure this is 'dist'
+    outDir: 'frontend/dist', // Make sure this is 'dist'
   },
 })
 // vite.config.js
